ac2e5d4e55496bc045803a8a6c975d5e9ad034ec
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-29 16:49:56 -0300

    containers ao fundo e botões d criar e entrar no jogo

--- lib/screen/game.dart
+++ lib/screen/game.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fiap_platform_channel/util/constants.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class WrapperCreator {
+  final bool creator;
+  final String nameGame;
+  WrapperCreator(this.creator, this.nameGame);
+}
 
 class Game extends StatefulWidget {
   @override
@@ -11,10 +19,88 @@ class _GameState extends State<Game> {
       fontSize: 75,
       color: Colors.white
   );
+  TextStyle textStyle36 = TextStyle(
+      fontSize: 36,
+      color: Colors.white
+  );
+
+  bool minhaVez;
+  WrapperCreator creator;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    ScreenUtil.init(
+        context,
+        width: 700,
+        height: 1400,
+        allowFontScaling: false);
+
+    return Scaffold(
+      //stack: pilha de widgets, sendo que o últio colocado fica sobre os demais
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: ScreenUtil().setWidth(550),
+                    height: ScreenUtil().setHeight(550),
+                    color: colorBackBlue1,
+                  ),
+                  Container(
+                    width: ScreenUtil().setWidth(150),
+                    height: ScreenUtil().setHeight(550),
+                    color: colorBackBlue2,
+                  )
+                ],
+              ),
+              Container(
+                width: ScreenUtil().setWidth(700),
+                height: ScreenUtil().setHeight(850),
+                color: colorBackBlue3,
+              )
+            ],
+          ),
+          Container(
+            height: ScreenUtil().setHeight(1400),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  creator == null ? Row(
+                    children: [
+                      buildButton("Criar", true),
+                      SizedBox(width: 10),
+                      buildButton("Entrar", false)
+                    ],
+                    mainAxisSize: MainAxisSize.min,
+                  ) : Text(
+                      minhaVez ? "Sua Vez!!" : "Aguarde Sua Vez!!!",
+                      style: textStyle36
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
+
+  //constrói as opções iniciais do jogo (criar jogo ou entrar em um jogo)
+  Widget buildButton(String label, bool owner) => Container(
+    width: ScreenUtil().setWidth(300),
+    child: OutlineButton(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          label,
+          style: textStyle36
+        ),
+      ),
+    ),
+  );
 
 }
